f4182f3ddb6301f576d251ded4ddc1da7c5869a7
Nicolas MEDDA <nicolas.medda@mynewsdesk.com>
2014-09-19 09:08:41 +0200

    Indentation fix

--- tests/todo-interface.js
+++ tests/todo-interface.js
@@ -10,105 +10,106 @@ var driver = new webdriver
   .withCapabilities(webdriver.Capabilities.chrome())
   .build();
 
-var expect = require('chai').expect;
+  var expect = require('chai').expect;
 
-test.describe('Todo App', function() {
-  test.before(function() {
-    driver.get('http://localhost:8000/html/todo.html');
-  });
+  test.describe('Todo App', function() {
+    test.before(function() {
+      // Clear the localStorage
+      driver.get('http://localhost:8000/html/todo.html');
+    });
 
-  test.after(function() {
-    driver.close();
-    driver.quit();
-  });
+    test.after(function() {
+      driver.close();
+      driver.quit();
+    });
 
-  test.it('Should add a todo on ENTER', function() {
-    var input = driver.findElement(By.css('#add-todo-form .input'));
-    input.sendKeys('a new todo', webdriver.Key.ENTER);
+    test.it('Should add a todo on ENTER', function() {
+      var input = driver.findElement(By.css('#add-todo-form .input'));
+      input.sendKeys('a new todo', webdriver.Key.ENTER);
 
-    getTodos().then(function(todos) {
+      getTodos().then(function(todos) {
         expect(todos).to.have.property('length').equal(1);
         assertTodoLabel(todos[0], 'a new todo');
       });
-  });
+    });
 
-  test.it('Should add a todo on click on the "Add" button', function() {
-    var input = driver.findElement(By.css('#add-todo-form .input'));
-    input.sendKeys('something else to do');
-    driver.findElement(By.css('#add-todo-form input[type="submit"]')).click();
+    test.it('Should add a todo on click on the "Add" button', function() {
+      var input = driver.findElement(By.css('#add-todo-form .input'));
+      input.sendKeys('something else to do');
+      driver.findElement(By.css('#add-todo-form input[type="submit"]')).click();
 
-    getTodos().then(function(todos) {
+      getTodos().then(function(todos) {
         expect(todos).to.have.property('length').equal(2);
         assertTodoLabel(todos[1], 'something else to do');
       });
-  });
+    });
 
-  test.it('Should remove a todo on click on the "Remove" button', function() {
-    var firstTodo = getFirstTodo();
+    test.it('Should remove a todo on click on the "Remove" button', function() {
+      var firstTodo = getFirstTodo();
 
-    firstTodo.findElement(By.css('.btn.remove')).click();
+      firstTodo.findElement(By.css('.btn.remove')).click();
 
-    getTodos().then(function(todos) {
+      getTodos().then(function(todos) {
         expect(todos).to.have.property('length').equal(1);
         assertTodoLabel(todos[0], 'something else to do');
       });
-  });
-
-  test.it('Should display the update form on click on the "Update" button', function() {
-    var firstTodo = getFirstTodo();
-
-    firstTodo.findElement(By.css('.btn.update')).click();
-
-    firstTodo.findElement(By.css('.todo-update')).isDisplayed().then(function(isDisplayed) {
-      expect(isDisplayed).to.be.true;
-    });
-  });
-
-  test.it('Should hide the update form on click on the "cancel" button', function() {
-    var firstTodo = getFirstTodo();
-
-    firstTodo.findElement(By.css('.btn.cancel-edit')).click();
-
-    firstTodo.findElement(By.css('.todo-update')).isDisplayed().then(function(isDisplayed) {
-      expect(isDisplayed).to.be.false;
     });
 
-  });
+    test.it('Should display the update form on click on the "Update" button', function() {
+      var firstTodo = getFirstTodo();
 
-  test.it('Should update the todo on enter', function() {
-    var firstTodo = getFirstTodo();
-    firstTodo.findElement(By.css('.btn.update')).click();
+      firstTodo.findElement(By.css('.btn.update')).click();
 
-    var updateInput = firstTodo.findElement(By.css('.todo-update input[name="todoName"]'));
-    updateInput.clear();
-    updateInput.sendKeys('change name', webdriver.Key.ENTER);
-
-    driver.findElement(By.css('.todo-item:first-child')).then(function(todo) {
-      assertTodoLabel(todo, 'change name');
+      firstTodo.findElement(By.css('.todo-update')).isDisplayed().then(function(isDisplayed) {
+        expect(isDisplayed).to.be.true;
+      });
     });
-  });
 
-  test.it('Should mark a todo as done', function() {
-    var firstTodo = getFirstTodo();
+    test.it('Should hide the update form on click on the "cancel" button', function() {
+      var firstTodo = getFirstTodo();
 
-    firstTodo.findElement(By.css('input[type="checkbox"]')).click();
-    
-    getFirstTodo().getAttribute('class').then(function(classNames) {
-      expect(classNames.split(' ')).to.include('done');
+      firstTodo.findElement(By.css('.btn.cancel-edit')).click();
+
+      firstTodo.findElement(By.css('.todo-update')).isDisplayed().then(function(isDisplayed) {
+        expect(isDisplayed).to.be.false;
+      });
+
     });
-  });
 
-  test.it('Should mark a todo as not done', function() {
-    var firstTodo = getFirstTodo();
+    test.it('Should update the todo on enter', function() {
+      var firstTodo = getFirstTodo();
+      firstTodo.findElement(By.css('.btn.update')).click();
 
-    firstTodo.findElement(By.css('input[type="checkbox"]')).click();
-    
-    getFirstTodo().getAttribute('class').then(function(classNames) {
-      expect(classNames.split(' ')).not.to.include('done');
+      var updateInput = firstTodo.findElement(By.css('.todo-update input[name="todoName"]'));
+      updateInput.clear();
+      updateInput.sendKeys('change name', webdriver.Key.ENTER);
+
+      driver.findElement(By.css('.todo-item:first-child')).then(function(todo) {
+        assertTodoLabel(todo, 'change name');
+      });
     });
-  });
 
-});
+    test.it('Should mark a todo as done', function() {
+      var firstTodo = getFirstTodo();
+
+      firstTodo.findElement(By.css('input[type="checkbox"]')).click();
+
+      getFirstTodo().getAttribute('class').then(function(classNames) {
+        expect(classNames.split(' ')).to.include('done');
+      });
+    });
+
+    test.it('Should mark a todo as not done', function() {
+      var firstTodo = getFirstTodo();
+
+      firstTodo.findElement(By.css('input[type="checkbox"]')).click();
+
+      getFirstTodo().getAttribute('class').then(function(classNames) {
+        expect(classNames.split(' ')).not.to.include('done');
+      });
+    });
+
+  });
 
 
 function getFirstTodo() {
